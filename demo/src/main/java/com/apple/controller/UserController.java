20c778253db3d68005dfe7d414d3935055308e85
GSL <13121514103@163.com>
2019-06-26 09:09:35 +0800

    返回值index//

---
+++ demo/src/main/java/com/apple/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.apple.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @RequestMapping("/index")
+    public String appleUser(){
+
+        return "index//";
+    }
+
+}
